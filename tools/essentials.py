fffa09c0331db9b9e85d48a9c20914a61baac943
Kinuseka <realkingseeker1089@gmail.com>
2024-06-09 01:09:05 +0800

    useful tools

---
+++ tools/essentials.py
@@ -0,0 +1,16 @@
+import re
+
+def remove_ansi_escape(text):
+    # 7-bit C1 ANSI sequences
+    ansi_escape = re.compile(r'''
+        \x1B  # ESC
+        (?:   # 7-bit C1 Fe (except CSI)
+            [@-Z\\-_]
+        |     # or [ for CSI, followed by a control sequence
+            \[
+            [0-?]*  # Parameter bytes
+            [ -/]*  # Intermediate bytes
+            [@-~]   # Final byte
+        )
+    ''', re.VERBOSE)
+    return ansi_escape.sub('', text)
