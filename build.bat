aac5f77a97b177672c49726e0a67174ae16524dc
Kinuseka <realkingseeker1089@gmail.com>
2024-06-09 01:04:21 +0800

    added checksum during build

--- build.bat
+++ build.bat
@@ -1,9 +1,20 @@
 @echo off
 python -m venv .venv
+set FileName=DBDRegion.exe
 
 call .venv\Scripts\activate
 where pip
+where python
 
-pip install -r requirements.txt
-python -m PyInstaller --noconfirm --onefile --windowed --icon "./res/image2.ico" --uac-admin --add-data "./res/*;res/" "./mainGUI.py" -n "DBDRegion.exe" --version-file "./res/version_file.txt"
+pip install -r requirements.txt --upgrade
+python -m PyInstaller --noconfirm --onefile --windowed --icon "./res/image2.ico" --uac-admin --add-data "./res/*;res/" "./mainGUI.py" -n %FileName% --version-file "./res/version_file.txt"
+certutil -hashfile ./dist/%FileName% SHA256 > temp.txt
+for /f "skip=1 tokens=*" %%i in (temp.txt) do (
+    echo %%i > ./dist/%FileName%_sha256checksum.txt
+    goto :break
+)
+:break
+del temp.txt
+echo Hash has been calculated and stored in ./dist/%FileName%_sha256checksum.txt
+
 pause
