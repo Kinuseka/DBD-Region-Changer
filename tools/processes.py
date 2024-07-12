e55403e5af9eaf9386bb8d46eca5ad8d8c982290
Kinuseka <realkingseeker1089@gmail.com>
2024-07-12 10:33:42 +0800

    improve debug console

--- tools/processes.py
+++ tools/processes.py
@@ -1,33 +1,52 @@
-from win32event import CreateMutex
-from win32api import CloseHandle, GetLastError
-from winerror import ERROR_ALREADY_EXISTS
+import win32event
+import win32api
+import winerror
+import win32console
+import win32gui
+import win32con
 from typing import List, Union
 from .essentials import remove_ansi_escape
-import win32console
 import io
 import sys
-
+import time
+import traceback
 
 class InstanceLimiter:
     def __init__(self):
         self.mutexname = "dcreg_{c60ca70d-59a9-43c6-bd96-44f755b45095}"
-        self.mutex = CreateMutex(None, False, self.mutexname)
-        self.lasterror = GetLastError()
+        self.mutex = win32event.CreateMutex(None, False, self.mutexname)
+        self.lasterror = win32api.GetLastError()
 
     def is_running(self):
-        return (self.lasterror == ERROR_ALREADY_EXISTS)
+        return (self.lasterror == winerror.ERROR_ALREADY_EXISTS)
 
     def close(self):
-        if not self.is_running and self.mutex:
-            CloseHandle(self.mutex)
+        win32api.CloseHandle(self.mutex)
+    
+    def __del__(self):
+        self.close()
 
 class ConsoleDetached(io.TextIOBase):
-    def __init__(self, split=None):
+    def __init__(self, split=None, title="Debug Console"):
         super().__init__()
-        win32console.FreeConsole()
-        win32console.AllocConsole()
-        self.stdconsole = win32console.GetStdHandle(win32console.STD_OUTPUT_HANDLE)
-        self.stdconsole.SetConsoleMode(7)
+        self.is_ready = False
+        self.is_exe = bool(sys.stdout)
+        try:
+            win32console.FreeConsole()
+            win32console.AllocConsole()
+            self.stdconsole = win32console.GetStdHandle(win32console.STD_OUTPUT_HANDLE)
+            self.stdconsole.SetConsoleMode(7)
+            win32gui.EnableMenuItem(win32gui.GetSystemMenu(win32console.GetConsoleWindow(), False), win32con.SC_CLOSE, win32con.MF_BYCOMMAND | win32con.MF_GRAYED)
+            win32console.SetConsoleTitle(title)
+            self.is_ready = True
+        except Exception as e:
+            # self.stdconsole.Detach()
+            self.last_exception = e
+            if not self.is_exe:
+                print(traceback.format_exc())
+                time.sleep(10)
+            else:
+                raise e
 
     def write(self, data: str):
         self.stdconsole.WriteConsole(data)
