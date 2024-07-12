4174e786f9526a6c89009c20979fa21a677e603a
Kinuseka <realkingseeker1089@gmail.com>
2024-07-12 10:32:15 +0800

    update window

--- models/window.py
+++ models/window.py
@@ -1,7 +1,9 @@
 from PyQt5.QtWidgets import QMessageBox
+from PyQt5 import QtCore
 class ErrorBox(QMessageBox):
-    def __init__(self, header, message, title="Error"):
+    def __init__(self, header, message, title="Error", ontop = True, parent = None):
         super().__init__()
+        self.parent = parent
         self.title = title
         self.header = header
         self.message = message
@@ -9,3 +11,5 @@ class ErrorBox(QMessageBox):
         self.setText(f'<b>{header}</b>')
         self.setInformativeText(message)
         self.setWindowTitle(title)
+        if ontop:
+            self.setWindowFlag(QtCore.Qt.WindowStaysOnTopHint)
